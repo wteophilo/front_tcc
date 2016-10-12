4a301b234e2a920175f3716c2ccb704ab1124548
Willian Teophilo <wteophilo@gmail.com>
2016-10-12 20:47:15 -0300

    Filtro CORS

--- src/main/java/br/com/wt/front/Boot.java
+++ src/main/java/br/com/wt/front/Boot.java
@@ -2,12 +2,14 @@ package br.com.wt.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @Controller
+@ComponentScan
 public class Boot
 {
 
