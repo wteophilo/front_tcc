9d34cf522139a16adde4f87c34223f3400194491
Willian Teophilo <wteophilo@gmail.com>
2016-10-29 19:42:43 -0200

    Adicionando Zuul

--- src/main/java/br/com/wt/front/Boot.java
+++ src/main/java/br/com/wt/front/Boot.java
@@ -2,13 +2,14 @@ package br.com.wt.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
+@EnableZuulProxy
 @ComponentScan
 public class Boot
 {
--- src/main/resources/static/js/config/configValue.js
+++ src/main/resources/static/js/config/configValue.js
@@ -1,5 +1,5 @@
 angular.module("app").constant("config", {
-  baseUrlAgenda: "http://localhost:8080/agenda",
+  baseUrlAgenda: "agenda",
   baseUrlLaboratorio: "http://localhost:8081/laboratorio",
   baseUrlHospitalar: "http://localhost:8082/unidadehospitalar"
 });
