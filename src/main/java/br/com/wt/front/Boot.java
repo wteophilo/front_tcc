2d67c9d27c64c40a30adbd1d70691b4685ccf874
Willian Teophilo <wteophilo@gmail.com>
2016-11-03 06:36:23 -0200

    Validações

--- src/main/java/br/com/wt/front/Boot.java
+++ src/main/java/br/com/wt/front/Boot.java
@@ -3,26 +3,30 @@ package br.com.wt.front;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.wt.front.config.PreFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
 @ComponentScan
-public class Boot
-{
+public class Boot {
 
-   public static void main(String[] args)
-   {
-      SpringApplication.run(Boot.class, args);
-   }
+	public static void main(String[] args) {
+		SpringApplication.run(Boot.class, args);
+	}
 
-   @RequestMapping("/poc_pucminas")
-   @ResponseBody
-   public String home()
-   {
-      return "index";
-   }
+	@RequestMapping("/poc_pucminas")
+	@ResponseBody
+	public String home() {
+		return "index";
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
 }
--- src/main/resources/static/js/controllers/LaboratorioController.js
+++ src/main/resources/static/js/controllers/LaboratorioController.js
@@ -20,5 +20,5 @@ angular.module('app').controller('laboratoriocontroller',function($scope,laborat
     };
 
 
-    carregaLaboratorio();
+    //carregaLaboratorio();
 });
