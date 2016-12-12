d4f1be7c00965f85d3856f69060e635802b7b086
Willian Teophilo <wteophilo@gmail.com>
2016-12-12 06:32:34 -0200

    inclusão travis

--- src/main/resources/static/js/config/configValue.js
+++ src/main/resources/static/js/config/configValue.js
@@ -1,15 +1,15 @@
 //Produção
-//angular.module("app").constant("config", {
-//  baseUrlAgenda: "https://agendamento.herokuapp.com/agenda",
-//  baseUrlDoador: "https://agendamento.herokuapp.com/doador",
-//  baseUrlLaboratorio: "https://labservice.herokuapp.com/laboratorio",
-//  baseUrlBolsa "https://labservice.herokuapp.com/bolsa"
-//});
+angular.module("app").constant("config", {
+  baseUrlAgenda: "https://agendamento.herokuapp.com/agenda",
+  baseUrlDoador: "https://agendamento.herokuapp.com/doador",
+  baseUrlLaboratorio: "https://labservice.herokuapp.com/laboratorio",
+  baseUrlBolsa: "https://labservice.herokuapp.com/bolsa"
+});
 
-//dev
+/*dev
 angular.module("app").constant("config", {
   baseUrlAgenda: "http://127.0.0.1:8080/agenda",
   baseUrlDoador: "http://127.0.0.1:8080/doador",
   baseUrlLaboratorio: "http://127.0.0.1:8081/laboratorio",
   baseUrlBolsa: "http://127.0.0.1:8081/bolsa"
-});
+});*/
